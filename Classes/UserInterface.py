f718cb77b3122410e30f64f17dcdb77df26646d2
Bernie <95599456+Unforgettableeternalproject@users.noreply.github.com>
2022-10-06 15:24:35 +0800

    Added menu and a lot more components 10/6

--- Classes/UserInterface.py
+++ Classes/UserInterface.py
@@ -13,42 +13,11 @@ class ui():
         pass
 
     def open_window(self):
-        #Tkinter的部分
+        #視窗介面
         self.win.title('OmniImaginer.exe')
         self.win.geometry('1000x563')
         self.win.resizable(False, False)
         self.win.iconbitmap('Bernie.ico')
-        ##分格-1
-        #self.upper = tk.Frame(self.win, width=1000, height=171)
-        #self.lower = tk.Frame(self.win, width=1000, height=392)
-        #self.upper.grid(column=0, row=0, padx=self.pad, pady=self.pad, sticky=self.align_mode)
-        #self.lower.grid(column=0, row=1, padx=self.pad, pady=self.pad, sticky=self.align_mode)
-        ##分格-2
-        #self.upl = tk.Frame(self.upper, width=561)
-        #self.upr = tk.Frame(self.upper, width=423)
-        #self.lol = tk.Frame(self.lower, width=561)
-        #self.PoI = tk.Frame(self.lower, width=423)
-        #self.upl.grid(column=0, row=0, padx=self.pad/2, pady=self.pad, sticky=self.align_mode)
-        #self.upr.grid(column=1, row=0, padx=self.pad/2, pady=self.pad/2, sticky=self.align_mode)
-        #self.lol.grid(column=0, row=0, padx=self.pad/2, pady=self.pad/2, sticky=self.align_mode)
-        #self.PoI.grid(column=1, row=0, padx=self.pad/2, pady=self.pad/2, sticky=self.align_mode)
-        ##分格-3
-        #self.LII = tk.Frame(self.upl, height=74)
-        #self.IUI = tk.Frame(self.upl, height=75)
-        #self.CI = tk.Frame(self.upr, width=99)
-        #self.L = tk.Frame(self.upr, width=99)
-        #self.EP = tk.Frame(self.lol, width=258, height=266)
-        #self.SD = tk.Frame(self.lol, width=295, height=62)
-        #self.PP = tk.Frame(self.lol, width=295, height=204)
-        #self.ExP = tk.Frame(self.lol, height=102)
-        #self.LII.grid(column=0, row=0, padx=self.pad/4, pady=self.pad/2, sticky=self.align_mode)
-        #self.IUI.grid(column=0, row=1, padx=self.pad/4, pady=self.pad/2, sticky=self.align_mode)
-        #self.CI.grid(column=0, row=0, padx=self.pad/4, pady=self.pad/4, sticky=self.align_mode)
-        #self.L.grid(column=1, row=0, padx=self.pad/4, pady=self.pad/4, sticky=self.align_mode)
-        #self.EP.grid(column=0, row=0, rowspan=2, padx=self.pad/4, pady=self.pad/4, sticky=self.align_mode)
-        #self.SD.grid(column=1, row=0, padx=self.pad/4, pady=self.pad/4, sticky=self.align_mode)
-        #self.PP.grid(column=1, row=1, padx=self.pad/4, pady=self.pad/4, sticky=self.align_mode)
-        #self.ExP.grid(column=0, row=2, columnspan=2, padx=self.pad/4, pady=self.pad/4, sticky=self.align_mode)
         #本地檔案導入方式(LII)
         self.promptL = tk.Label(text="選取本地檔案", bg="grey", fg="white", height=2, width=15)
         self.entryL = tk.Text(height=2, width=45)
@@ -65,26 +34,55 @@ class ui():
         self.promptU.place(x=25,y=77)
         self.entryU.place(x=150,y=80)
         self.btnU.place(x=485,y=82)
-        #self.promptU.grid(column=0, row=1)
-        #self.entryU.grid(column=1, row=1, padx=8)
-        #self.btnU.grid(column=2, row=1)
         #雲端導入方式(CI)
-        self.promptC = tk.Label(text="或者...從雲端導入")
-        self.btnGD = tk.Button(text="Google Drive", height=2, width=20)
-        self.btnDB = tk.Button(text="Dropbox", height=2, width=20)
-        #self.promptC.grid(column=0, row=0, columnspan=2, padx=40)
-        #self.btnGD.grid(column=0, row=1, columnspan=2, padx=40)
-        #self.btnDB.grid(column=0, row=2, columnspan=2, padx=40)
+        self.GDicon = ImageTk.PhotoImage(Image.open('Drive.png').resize((50,50)))
+        self.DBicon = ImageTk.PhotoImage(Image.open('Dropbox.png').resize((50,50)))
+        self.promptC = tk.Label(text="或者...從雲端導入", bg="grey", fg="white", height=2, width=20)
+        self.btnGD = tk.Button(text="Google Drive", image=self.GDicon)
+        self.btnDB = tk.Button(text="Dropbox", image = self.DBicon)
+        self.promptC.place(x=600, y=15)
+        self.btnGD.place(x=610, y=60)
+        self.btnDB.place(x=680, y=60)
         #浮水印(L)
-        self.oimg= (Image.open("uep.png"))
-        self.img = ImageTk.PhotoImage(self.oimg.resize((100,120), Image.ANTIALIAS))
-        self.label = tk.Label(image=self.img)
-        #self.label.grid(column=0, row=0, columnspan=2)
+        #self.img= ImageTk.PhotoImage(Image.open("uep.png").resize((100,120)))
+        self.label = tk.Label(text="浮水印預定放置區塊",bg="grey", fg="white", height=5, width=20)
+        self.label.place(x=800,y=25)
         #效果處理器(EP)
         #方位處理器(PP)
         #尺寸動態顯示(SD)
+        #圖片資訊顯示(ID)
         #圖片預覽(PoI)
         #輸出(ExP)
+
+        #菜單
+        self.menu = tk.Menu(self.win, background='red')
+
+        self.win.config(menu=self.menu)
+        self.file = tk.Menu(self.menu, tearoff=0)
+        self.file.add_command(label='顯示範例') #程式中顯示範例圖片檔的預覽
+        self.file.add_command(label='重置') #跳出視窗顯示警告，並詢問是否真的要重置
+
+        self.window = tk.Menu(self.menu, tearoff=0)
+        self.window.add_command(label='步驟紀錄') #跳出新視窗，顯示步驟紀錄
+        self.window.add_command(label='開發者資訊') #跳出新視窗，顯示開發者資訊
+
+        self.view = tk.Menu(self.menu, tearoff=0)
+        self.view_options = tk.Menu(self.view, tearoff=0)
+        self.view_options.add_command(label='小') #變更為小視窗
+        self.view_options.add_command(label='中等') #變更為中等視窗
+        self.view_options.add_command(label='大') #變更為大視窗
+        self.view.add_cascade(label='更改視窗大小', menu=self.view_options) #給予更改視窗比例的選項
+
+        self.help = tk.Menu(self.menu, tearoff=0)
+        self.window.add_command(label='功能介紹') #跳出新視窗，顯示一個功能介紹的畫面(Help)
+        self.help.add_command(label='聯絡開發者') #跳出新視窗，內嵌開發者聯絡資訊(直接寄信?)
+
+        self.menu.add_cascade(label='檔案', menu=self.file)
+        self.menu.add_cascade(label='視窗', menu=self.window)
+        self.menu.add_cascade(label='顯示', menu=self.view)
+        self.menu.add_cascade(label='幫助', menu=self.help)
+
+        #運行程式
         self.win.mainloop()
 
 
