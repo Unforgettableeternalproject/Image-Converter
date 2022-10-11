53ac8f37af9570807892401596f15e39a289865a
Bernie <95599456+Unforgettableeternalproject@users.noreply.github.com>
2022-10-11 15:11:05 +0800

    Added HSV slide and some buttons 10/11

--- Classes/UserInterface.py
+++ Classes/UserInterface.py
@@ -22,6 +22,9 @@ class ui():
         self.entryL['state'] = DISABLED
         return file_path
 
+    def Update(self, scale, text):
+        pass
+
     def __init__(self) -> None:
         self.win = tk.Tk()
         self.basepath = path.dirname(path.realpath(__file__))
@@ -35,6 +38,7 @@ class ui():
         self.win.geometry('1000x563')
         self.win.resizable(False, False)
         self.win.iconbitmap('Bernie.ico')
+
         #本地檔案導入方式(LII)
         self.promptL = tk.Label(text="選取本地檔案", bg="grey", fg="white", height=2, width=15)
         self.entryL = tk.Text(height=2, width=45)
@@ -43,17 +47,17 @@ class ui():
         self.btnL.pack()
         #self.labelL = tk.Label( text = file_path)
         #self.labelL.pack()
-        self.promptL.place(x=25,y=27)
-        self.entryL.place(x=150,y=30)
-        self.btnL.place(x=485,y=32)
+        self.promptL.place(x=25, y=27)
+        self.entryL.place(x=150, y=30)
+        self.btnL.place(x=485, y=32)
         #網路檔案導入方式(IUI)
         self.promptU = tk.Label(text="導入網路檔案", bg="grey", fg="white", height=2, width=15)
         self.entryU = tk.Text(height=2, width=45)
         self.entryU['state'] = DISABLED
         self.btnU = tk.Button(text="...", height=1, width=4)
-        self.promptU.place(x=25,y=77)
-        self.entryU.place(x=150,y=80)
-        self.btnU.place(x=485,y=82)
+        self.promptU.place(x=25, y=77)
+        self.entryU.place(x=150, y=80)
+        self.btnU.place(x=485, y=82)
         #雲端導入方式(CI)
         self.GDicon = ImageTk.PhotoImage(Image.open('Drive.png').resize((50,50)))
         self.DBicon = ImageTk.PhotoImage(Image.open('Dropbox.png').resize((50,50)))
@@ -66,8 +70,24 @@ class ui():
         #浮水印(L)
         #self.img= ImageTk.PhotoImage(Image.open("uep.png").resize((100,120)))
         self.label = tk.Label(text="浮水印預定放置區塊",bg="grey", fg="white", height=5, width=20)
-        self.label.place(x=800,y=25)
+        self.label.place(x=800, y=25)
         #效果處理器(EP)
+            #HSV滑桿的部分
+        self.H_label = tk.Label(text="色相:").place(x=15, y=169)
+        self.S_label = tk.Label(text="飽和度:").place(x=4, y=209)
+        self.V_label = tk.Label(text="明度:").place(x=15, y=249)
+        self.H_slider = tk.Scale(from_=0, to=360, length=200, orient=tk.HORIZONTAL)
+        self.S_slider = tk.Scale(from_=0, to=100, length=200, orient=tk.HORIZONTAL)
+        self.V_slider = tk.Scale(from_=0, to=100, length=200, orient=tk.HORIZONTAL)
+        self.H_entry = tk.Entry(width=4, state=DISABLED).place(x=260, y=170) #Entry部分之後會做數值同步
+        self.S_entry = tk.Entry(width=4, state=DISABLED).place(x=260, y=210)
+        self.V_entry = tk.Entry(width=4, state=DISABLED).place(x=260, y=250)
+        self.H_slider.place(x=50, y=150)
+        self.S_slider.place(x=50, y=190)
+        self.V_slider.place(x=50, y=230)
+            #侵蝕、膨脹的部分
+        self.erodebtn = tk.Button(text="侵蝕++", height=2, width=7).place(x=50, y=280)
+        self.dilatebtn = tk.Button(text="膨脹++", height=2, width=7).place(x=50, y=330)
         #方位處理器(PP)
         #尺寸動態顯示(SD)
         #圖片資訊顯示(ID)
@@ -101,7 +121,6 @@ class ui():
         self.menu.add_cascade(label='視窗', menu=self.window)
         self.menu.add_cascade(label='顯示', menu=self.view)
         self.menu.add_cascade(label='幫助', menu=self.help)
-
         #運行程式
         self.win.mainloop()
 
