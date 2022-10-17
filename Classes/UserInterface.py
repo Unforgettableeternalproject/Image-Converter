21a44ea9ae0cb571df9d083e95cb48c959b71b9c
Bernie <95599456+Unforgettableeternalproject@users.noreply.github.com>
2022-10-17 17:09:58 +0800

    Done with EP, SD, and PP 10/17

--- Classes/UserInterface.py
+++ Classes/UserInterface.py
@@ -4,7 +4,7 @@ import tkinter as tk
 from PIL import Image,ImageTk
 from tkinter import filedialog
 from tkinter import ttk
-import os
+from idlelib.tooltip import Hovertip
 
 file_path="a"
 
@@ -74,15 +74,12 @@ class ui():
         self.H_label = tk.Label(text="色相:").place(x=15, y=169)
         self.S_label = tk.Label(text="飽和度:").place(x=4, y=209)
         self.V_label = tk.Label(text="明度:").place(x=15, y=249)
-        self.H_slider = tk.Scale(from_=0, to=360, length=200, orient=tk.HORIZONTAL)
-        self.S_slider = tk.Scale(from_=0, to=100, length=200, orient=tk.HORIZONTAL)
-        self.V_slider = tk.Scale(from_=0, to=100, length=200, orient=tk.HORIZONTAL)
+        self.H_slider = tk.Scale(from_=0, to=360, length=200, orient=tk.HORIZONTAL).place(x=50, y=150)
+        self.S_slider = tk.Scale(from_=0, to=100, length=200, orient=tk.HORIZONTAL).place(x=50, y=190)
+        self.V_slider = tk.Scale(from_=0, to=100, length=200, orient=tk.HORIZONTAL).place(x=50, y=230)
         self.H_entry = tk.Entry(width=4, state=DISABLED).place(x=260, y=170) #Entry部分之後會做數值同步
         self.S_entry = tk.Entry(width=4, state=DISABLED).place(x=260, y=210)
         self.V_entry = tk.Entry(width=4, state=DISABLED).place(x=260, y=250)
-        self.H_slider.place(x=50, y=150)
-        self.S_slider.place(x=50, y=190)
-        self.V_slider.place(x=50, y=230)
             #侵蝕、膨脹的部分
         self.erodebtn = tk.Button(text="侵蝕++", height=2, width=7).place(x=50, y=280)
         self.dilatebtn = tk.Button(text="膨脹++", height=2, width=7).place(x=50, y=330)
@@ -96,7 +93,39 @@ class ui():
         self.clist = ttk.Combobox(width=17, state="readonly", value=["無", "Boxblur", "Blur", "Medianblur", "Bilateral", "Gaussian"]).place(x=85, y=385)
             #灰階的部分
         self.gsck = tk.Checkbutton(text="灰階").place(x=235, y=383)
+        #尺寸動態顯示(SD)
+        self.distext = tk.Label(text="原始圖片尺寸:").place(x=320, y=150)
+        self.display = tk.Text(height=1, width=15, state="disabled").place(x=325, y=173)
         #方位處理器(PP)
+            #縮放的部分
+        self.fixedscale = tk.Checkbutton(text="固定比例")
+        self.fixedscale.select()
+        self.zlabel = tk.Label(text="縮放:").place(x=320, y=195)
+        self.zoom = tk.Scale(from_=1, to=100, length=160, orient=tk.HORIZONTAL).place(x=320, y=215)
+        self.Z_entry = tk.Entry(width=4, state=DISABLED).place(x=490, y=235)
+        self.ztt = tk.Button(text="?", relief=tk.SUNKEN, height=1)
+        self.tp = Hovertip(self.ztt,'當固定比例被開啟時才可用')
+        self.relabel = tk.Label(text="自訂尺寸縮放:").place(x=320, y=260)
+        self.height = tk.Text(height=1, width=7).place(x=325, y=285)
+        self.x = tk.Label(text="x").place(x=385, y=283)
+        self.width = tk.Text(height=1, width=7).place(x=403, y=285)
+        self.reset = tk.Button(text="重置尺寸").place(x=463, y=280)
+        self.fixedscale.place(x=450, y=169)
+        self.ztt.place(x=360, y=194)
+            #旋轉的部分
+        self.rolabel = tk.Label(text="預設旋轉:").place(x=320, y=308)
+        self.zero =tk.Radiobutton(text="不旋轉", value=1)
+        self.nighty = tk.Radiobutton(text="旋轉90度", value=2).place(x=420, y=330)
+        self.horiflip = tk.Radiobutton(text="旋轉180度", value=3).place(x=320, y=355)
+        self.twoseventy = tk.Radiobutton(text="旋轉270度", value=4).place(x=420, y=355)
+        self.crolabel = tk.Label(text="自訂旋轉角度:").place(x=320, y=385)
+        self.croinput = tk.Text(height=1, width=4).place(x=405, y=387)
+        self.degree = tk.Label(text="度").place(x=435, y=385)
+        self.dlist = ttk.Combobox(width=7, state="readonly", value=["順時針", "逆時針"])
+        self.dlist.current(0)
+        self.zero.select()
+        self.zero.place(x=320, y=330)
+        self.dlist.place(x=455, y=385)
         #尺寸動態顯示(SD)
         #圖片資訊顯示(ID)
         #圖片預覽(PoI)
