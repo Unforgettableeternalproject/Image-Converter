9e99f4d5370d88e3c42924572a8db2d5506983ba
Bernie <ptyc4076@gmail.com>
2023-06-14 00:51:57 +0800

    Attempt 1 to package into a executable. 06/14

--- Main.py
+++ Main.py
@@ -2,6 +2,16 @@
 #from ctypes import windll
 #windll.shcore.SetProcessDpiAwareness(1)
 
+def source_path(relative_path):
+    if getattr(sys, 'frozen', False):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+ 
+cd = source_path('')
+os.chdir(cd)
+
 def checkReqPackages():
     with open("requirements.txt", mode = "r", encoding = "utf-8") as file:
         for i in file.readlines():
