bae5872c755eceedceb775d5dc1845a3b7e23bb8
Bernie <ptyc4076@gmail.com>
2025-05-07 23:06:15 +0800

    Removed unnecessary files. 05/07

--- Classes/EffectProcessor.py
+++ Classes/EffectProcessor.py
@@ -2,6 +2,7 @@
 import cv2
 import Classes.UserInterface as UI
 
+u = UI.ui()
 class eff_pro():
     def __init__(self) -> None:
         pass
@@ -10,19 +11,19 @@ class eff_pro():
         image = cv2.imread("Preview.png")
         image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-        newH = UI.ui.H_slider.get()
+        newH = u.H_slider.get()
 
         image[:, :, 0] = newH
 
         out = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
         cv2.imwrite("Preview.png", out)
-        UI.ui.updatePic()
+        u.updatePic()
 
     #依照滑桿的值改S
     def changeS(event):
         image = cv2.imread("Preview.png")
         image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        newS = UI.ui.S_slider.get()
+        newS = uS_slider.get()
             
         UI.ui.S_slider.set(image[:, :, 1])
             
